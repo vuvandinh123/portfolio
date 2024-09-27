1b72bc127c996c0462ff7b5d3d58812c56908f08
vuvandinh123 <vuvandinh203@gmail.com>
2024-09-27 23:54:16 +0700

    fix cros origin

--- src/app/api/note/route.ts
+++ src/app/api/note/route.ts
@@ -20,7 +20,7 @@ export async function GET(request: Request) {
             page,
             limit,
         }
-        const posts = await NoteModel.find({}).skip(skip).limit(limit).sort({date: -1}).exec();
+        const posts = await NoteModel.find({}).skip(skip).limit(limit).sort({ date: -1 }).exec();
         return ResponseSuccess({ data: posts, message: "Lấy dữ liệu thành công", options });
     } catch (error) {
         console.error(error);
@@ -28,13 +28,29 @@ export async function GET(request: Request) {
     }
 }
 
+const allowedOrigins = ['chrome-extension://acibknjhjbdcikjllgmconchgijmlpdh'];
 export async function POST(request: Request) {
+    const origin = request.headers.get('origin') || '';
     try {
+        if (!allowedOrigins.includes(origin)) {
+            return ResponseError({ message: "Không có quyền truy cập", status: 401 });
+        }
+        // Lấy token từ cookie
+        const token = (request as any).cookies.get('token')?.value;
+        if (!token) {
+            return ResponseError({ message: "Token không hợp lệ", status: 401 });
+        }
         await database();
-        
         const body = await request.json();
+        const headers = {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': origin,
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Access-Control-Allow-Credentials': 'true',
+        }
         const result = await NoteModel.insertMany([body]);
-        return ResponseSuccess({ data: result, message: "Tạo mới thành công", status: CREATED_SUCCESS });
+        return ResponseSuccess({ data: result, message: "Tạo mới thành công", status: CREATED_SUCCESS, headers: { ...headers } });
     } catch (error) {
         console.error(error);
         return ResponseError({ message: (error as Error).message });
